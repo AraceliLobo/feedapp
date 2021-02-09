23579ad928f54eb856d63645cd3197572c1d9c13
chicano85 <chicano.cano@gmail.com>
2021-02-09 12:02:31 +0100

    page structure

--- src/components/pages/GeneratePIN.js
+++ src/components/pages/GeneratePIN.js
@@ -1,1 +1,26 @@
+import "../../stylesheets/pages/_generatePIN.scss";
+import HomeLink from "../elements/HomeLink";
+import BtnLink from "../elements/BtnLink";
+import Footer from "../elements/Footer";
 
+const GeneratePIN = () => {
+  return (
+    <>
+      <main className="mainGeneratePIN">
+        <HomeLink></HomeLink>
+        <div className="inputContainer"></div>
+        <div className="btnContainer">
+          <BtnLink
+            to="/ViewFeedback"
+            title="Generar el pin de la sesión"
+            text="Generar el pin de la sesión"
+            className="two"
+          ></BtnLink>
+        </div>
+      </main>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default GeneratePIN;
